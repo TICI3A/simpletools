a68d9dc653bf97bb2e238855ab4d2e17bb1af15a
darfig <log.dariel@gmail.com>
2021-03-12 20:41:44 +0100

    addded

---
+++ changehostname.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# **********************
+# changehostname
+# 
+#
+#
+# v.2021.03.12 ubuntu 20.04 test
+# ***********************
+
+function main
+{
+    oldhost=$HOSTNAME
+    newhost=$1
+    echo "actual hostname $oldhost"
+    sudo hostnamectl set-hostname $newhost
+    sudo sed -i "s/${oldhost}/${newhost}/" /etc/hosts
+    echo "actual hostname $HOSTNAME"
+    echo "system go to reboot in 5 seg"
+    sleep 5
+    sudo reboot
+}
+
+
+if [ "$#" -lt 1 ]
+then
+  echo "Usage: "
+  echo "      $0 newhostname"
+  exit 85
+else
+  main $1
+fi
+
+exit 0
